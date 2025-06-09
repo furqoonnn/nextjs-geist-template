0397ba138b8950a68e5558022343a06c7130c348
furqoonnn <furqoonn@gmail.com>
2025-06-09 16:37:55 +0700

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1749461874410

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/node_modules_4e506e48._.js",
+  "static/chunks/src_bbf9a0e0._.js"
+],
+    source: "dynamic"
+});
