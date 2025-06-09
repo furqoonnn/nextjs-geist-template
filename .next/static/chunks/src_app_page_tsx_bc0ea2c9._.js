68f878e7488573d6f6bc6f75680219507e859692
furqoonnn <furqoonn@gmail.com>
2025-06-09 16:37:20 +0700

    Add .next/static/chunks/src_app_page_tsx_bc0ea2c9._.js 1749461839645

---
+++ .next/static/chunks/src_app_page_tsx_bc0ea2c9._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_components_2e578721._.js"
+],
+    source: "dynamic"
+});
